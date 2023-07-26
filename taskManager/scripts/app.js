2e3f843ee2f270e7277516bf90a31b6198e1d557
rickyarray <135184372+rickyarray@users.noreply.github.com>
2023-07-26 16:09:03 -0700

    Add files via upload
    Updated version of TaskManager

--- taskManager/scripts/app.js
+++ taskManager/scripts/app.js
@@ -2,9 +2,69 @@ let isImportant = false;
 let isVisable = true;
 
 function saveTask(){
-    
-    console.log("Saving task");
+    //get values
+    const title = $("#txtTitle").val();
+    const description = $("#txtDescription").val();
+    const color = $("#selColor").val();
+    const date = $("#selDate").val();
+    const status = $("#selStatus").val();
+    const budget =$("#txtBudget").val();
+
+
+    //build object
+    let taskSave = new Task(isImportant,title,description,color,date,status,budget);
+    console.log(taskSave);
+
+
+    //save to server
+    $.ajax({
+        type: "POST",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+        data:JSON.stringify(taskSave),
+        contentType: "application/json",
+        success: function(response) {
+            console.log(response);
+        //display the task & then clear form
+            displayTask(taskSave);
+        },
+        error: function(error){
+            console.log(error);
+            alert("something went wrong");
+        }
+    });
+
+
+    clearForm();
 }
+
+function clearForm(){
+    $("#txtTitle").val('')
+    $("#txtDescription").val('')
+    $("#selDate").val('')
+    $("#selStatus").val('')
+    $("#txtBudget").val('');
+
+
+}
+
+function displayTask(task){
+    let syntax = `<div class='task' style='border-color:${task.color}'>
+
+    <div class='info'>
+    <h5>${task.title}</h5>
+    <p>${task.description}</p>
+    </div>
+
+    <label class='status'>${task.status}</label>
+    
+    <div class='date-budget'>
+    <label>${task.startDate}</label>
+    <label>${task.budget}</label>
+    </div>`;
+
+    $(".pending-tasks").append(syntax);
+}
+
 
 function toggleImportant(){
 
@@ -38,17 +98,60 @@ function toggleVisibility(){
 }
 
 
+function testRequest() 
+{
+    $.ajax({
+        type: "GET",
+        url: "http://fsdiapi.azurewebsites.net/",
+        success: function(response) {
+            console.log(response);
+        },
+        error: function(error){
+            console.log(error);
+        }
+    });
+}
+
     
+// Create a GET request function to "http://fsdiapi.azurewebsites.net/api/tasks"
+function loadTask() {  
+    $.ajax({
+        type: "GET",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks",
+        
+        success: function(response){
+            let data = JSON.parse(response);
+            for(let i=0; i< data.length; i++) {
+                let task = data[i];
+                if(task.title == "Market"){
+                    displayTask(task);
+                }
+            }
+            console.log(response); // Console log the request from server
+            console.log(data);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    })
+}  
+
+
+//find the elements in the server that contains title=Market
+//render only those elements in the taskManager Panel
+
+
 function init(){
     console.log("task manager");
 
     //load data
+    loadTask();
+
 
     //assign events
     $("#btnSave").click(saveTask)
     $("#iImportant").click(toggleImportant)
-
-$("#btnDetails").click(toggleVisibility);
+    $("#btnDetails").click(toggleVisibility);
 }
 
 window.onload = init;
---
+++ taskManager/scripts/task.js
@@ -0,0 +1,15 @@
+
+class Task{
+
+    constructor(important, title, description, color, startDate, status, budget){
+        this.important = important;
+        this.title = title;
+        this.description = description;
+        this.color = color;
+        this.startDate = startDate;
+        this.status = status;
+        this.budget = budget;
+
+        this.name = "Ricky";
+    }
+}
